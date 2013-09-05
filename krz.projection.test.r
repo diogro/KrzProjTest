54da6bc2dc78c4102d6e9d07d9b12592b6cf80c9
diogro <diogro@gmail.com>
2013-09-05 17:08:56 -0300

    Comparing Random matrices using projections

--- krz.projection.test.r
+++ krz.projection.test.r
@@ -1,4 +1,12 @@
 source("~/projects/lem-usp-R/matrix.func.r")
+require(plyr)
+require(ggplot2)
+require(reshape2)
+library(doMC)
+library(foreach)
+registerDoMC(10)
+
+
 p = 39 #Traits
 n = 1 #Matrices
 sizematrices = qr.Q(qr(matrix(c(rep(1, p), rnorm(p*(p-1))), p, p)))
@@ -11,6 +19,13 @@ for(i in 1:(n-1)){
     current.mat = qr.Q(qr(matrix(rnorm(p*p), p, p)))
     nosizematrices = rbind(nosizematrices, current.mat)
 }
-write.csv(sizematrices, "size-39t-eigen-vectors.csv")
-write.csv(nosizematrices, "nosize-39t-eigen-vectors.csv")
+#write.csv(sizematrices, "size-39t-eigen-vectors.csv")
+#write.csv(nosizematrices, "nosize-39t-eigen-vectors.csv")
+
+file.list = dir("./random_matrices/", "matrix-")
+random.matrices = llply(file.list, function(x) as.matrix(read.table(paste("./random_matrices", x, sep = "/"), sep=',')))
+CompareToN <- function(n) laply(random.matrices[-n], function(x) {KrzProjection(x, random.matrices[[n]])[1]}, .parallel = T)
+comparisons  <- alply(1:100, 1, CompareToN)
+comparisons  <-  melt(comparisons)
+qplot(value, data=comparisons, geom="histogram")
 
