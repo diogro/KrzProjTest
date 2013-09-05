776e87148cb928c1852e4f543a24155aa1cc5b03
diogro <diogro@gmail.com>
2013-09-05 18:43:28 -0300

    Split stuff

--- krz.projection.test.r
+++ krz.projection.test.r
@@ -7,22 +7,6 @@ library(doMC)
 library(foreach)
 registerDoMC(10)
 
-
-p = 39 #Traits
-n = 1 #Matrices
-sizematrices = qr.Q(qr(matrix(c(rep(1, p), rnorm(p*(p-1))), p, p)))
-for(i in 1:(n-1)){
-    current.mat = qr.Q(qr(matrix(c(rep(1, p), rnorm(p*(p-1))), p, p)))
-    sizematrices = rbind(sizematrices, current.mat)
-}
-nosizematrices = qr.Q(qr(matrix(rnorm(p*p), p, p)))
-for(i in 1:(n-1)){
-    current.mat = qr.Q(qr(matrix(rnorm(p*p), p, p)))
-    nosizematrices = rbind(nosizematrices, current.mat)
-}
-#write.csv(sizematrices, "size-39t-eigen-vectors.csv")
-#write.csv(nosizematrices, "nosize-39t-eigen-vectors.csv")
-
 #file.list = dir("./random_matrices/", "matrix-.+.csv")
 #random.matrices = llply(file.list,
                         #function(x) as.matrix(read.table(paste("./random_matrices",
---
+++ random.eigen.vectors.r
@@ -0,0 +1,15 @@
+
+p = 39 #Traits
+n = 1 #Matrices
+sizematrices = qr.Q(qr(matrix(c(rep(1, p), rnorm(p*(p-1))), p, p)))
+for(i in 1:(n-1)){
+    current.mat = qr.Q(qr(matrix(c(rep(1, p), rnorm(p*(p-1))), p, p)))
+    sizematrices = rbind(sizematrices, current.mat)
+}
+nosizematrices = qr.Q(qr(matrix(rnorm(p*p), p, p)))
+for(i in 1:(n-1)){
+    current.mat = qr.Q(qr(matrix(rnorm(p*p), p, p)))
+    nosizematrices = rbind(nosizematrices, current.mat)
+}
+#write.csv(sizematrices, "size-39t-eigen-vectors.csv")
+#write.csv(nosizematrices, "nosize-39t-eigen-vectors.csv")
