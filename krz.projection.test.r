775c8d832bd3724a131828a5cf9ca34a4da88493
diogro <diogro@gmail.com>
2013-09-05 18:36:28 -0300

    Tests

--- krz.projection.test.r
+++ krz.projection.test.r
@@ -1,5 +1,6 @@
 source("~/projects/lem-usp-R/matrix.func.r")
 require(plyr)
+require(qdata)
 require(ggplot2)
 require(reshape2)
 library(doMC)
@@ -22,10 +23,26 @@ for(i in 1:(n-1)){
 #write.csv(sizematrices, "size-39t-eigen-vectors.csv")
 #write.csv(nosizematrices, "nosize-39t-eigen-vectors.csv")
 
-file.list = dir("./random_matrices/", "matrix-")
-random.matrices = llply(file.list, function(x) as.matrix(read.table(paste("./random_matrices", x, sep = "/"), sep=',')))
-CompareToN <- function(n) laply(random.matrices[-n], function(x) {KrzProjection(x, random.matrices[[n]])[1]}, .parallel = T)
-comparisons  <- alply(1:100, 1, CompareToN)
-comparisons  <-  melt(comparisons)
-qplot(value, data=comparisons, geom="histogram")
+#file.list = dir("./random_matrices/", "matrix-.+.csv")
+#random.matrices = llply(file.list,
+                        #function(x) as.matrix(read.table(paste("./random_matrices",
+                                                               #x,
+                                                               #sep = "/"),
+                                                         #sep=',')))
+
+file.list = dir("./random_matrices/", "matrix-.+.xls")
+random.matrices = llply(file.list,
+                        function(x) as.matrix(read.xls(paste("./random_matrices",
+                                                               x,
+                                                               sep = "/"), header=F)))
+
+CompareToNProj <- function(n) laply(random.matrices[-n], function(x) {KrzProjection(x, random.matrices[[n]])[1]}, .parallel = T)
+comparisons.proj  <- alply(1:100, 1, CompareToNProj, .progress="text")
+comparisons.proj  <-  melt(comparisons.proj)
+qplot(value, data=comparisons.proj, geom="histogram")
+
+CompareToNCor <- function(n) laply(random.matrices[-n], function(x) {KrzCor(x, random.matrices[[n]])}, .parallel = T)
+comparisons.cor  <- alply(1:100, 1, CompareToNCor, .progress="text")
+comparisons.cor  <-  melt(comparisons.cor)
+qplot(value, data=comparisons.cor, geom="histogram")
 
