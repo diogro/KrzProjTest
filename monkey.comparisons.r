0d6d8323937e8bf3e4da74f81e72ee393156b663
diogro <diogro@gmail.com>
2014-06-10 14:32:10 -0300

    Random Matrix Significance

--- monkey.comparisons.r
+++ monkey.comparisons.r
@@ -1,61 +1,41 @@
-source("~/projects/lem-usp-R/matrix.func.r")
-require(plyr)
 require(gdata)
 require(ggplot2)
 require(reshape2)
 library(doMC)
-library(foreach)
 registerDoMC(12)
+library(Morphometrics)
 
-load("~/Dropbox/labbio/cov_bayes_data/nwm.Rdata")
+load("~/Dropbox/labbio/cov_bayes_data/Rdatas/nwm.Rdata")
 cov.matrices = lapply(nwm.matrices, function(x) x[[1]])
 raw.hipot = read.table("./hipoteses.funcionais.csv", header = F, sep = '\t', as.is = T)
 hipoteses = list(Neuroface = as.matrix(raw.hipot[1:39,1:39]), All = as.matrix(raw.hipot[40:78,1:39]))
 mat.list = c(cov.matrices, hipoteses)
 
-MultiKrzProjection <- function(mat.list,
-                               ret.dim.1 = NULL, ret.dim.2 = NULL,
-                               num.cores = 1,
-                               full.results = F){
-    require(plyr)
-    require(reshape2)
-    if (num.cores > 1) {
-        library(doMC)
-        library(foreach)
-        registerDoMC(num.cores)
-        parallel = TRUE
-    }
-    else
-        parallel = FALSE
-    if(full.results){
-    CompareToNProj <- function(n) llply(mat.list,
-                                        function(x) {KrzProjection(x,
-                                                                   mat.list[[n]],
-                                                                   ret.dim.1, ret.dim.2)},
-                                        .parallel = parallel)
-    }
-    else{
-    CompareToNProj <- function(n) llply(mat.list[names(mat.list) != n],
-                                        function(x) {KrzProjection(x,
-                                                                   mat.list[[n]],
-                                                                   ret.dim.1, ret.dim.2)[1]},
-                                        .parallel = parallel)
-    }
-    comparisons.proj  <- llply(names(mat.list),
-                               CompareToNProj,
-                               .progress="text", .parallel = parallel)
-    if(full.results){
-        names(comparisons.proj) = names(mat.list)
-        return(comparisons.proj)
-    }
-    else{
-        comparisons.proj <- melt(comparisons.proj)
-        comparisons.proj[,4] = names(mat.list)[(comparisons.proj[,4])]
-    }
-    return(comparisons.proj[,-2])
+comparisons.cor = KrzCor(cov.matrices)
+comparisons.proj = KrzProjection(cov.matrices, num.cores = 12, full.results = F)
+comp = melt(data.frame(comparisons.proj))
+qplot(value, data = comp, geom = "histogram")
+
+###########################
+# Significance Testing
+###########################
+
+## Null hipotesis is dissimilarity
+
+## 1) Random Matrices
+
+x <- RandomMatrix(10)
+y <- RandomMatrix(10)
+x <- cov.matrices[[1]]
+y <- cov.matrices[[2]]
+KPS_Random <- function(x, y, iterations = 100){
+    obs_sim <- KrzProjection(x, y)[[1]]
+    dx <- diag(x)
+    dy <- diag(y)
+    rand_mats_x <- RandomMatrix(dim(x)[[1]], iterations, min(dx), max(dx))
+    rand_mats_y <- RandomMatrix(dim(y)[[1]], iterations, min(dy), max(dy))
+    random_comps <- unlist(Map(function(x, y) KrzProjection(x, y)[[1]], rand_mats_x, rand_mats_y))
+    significance <- sum(random_comps > obs_sim)/iterations
+    return(c(SharedVariance = obs_sim, Prob = significance))
 }
 
-
-comparisons.cor = MultiKrzCor(cov.matrices)
-comparisons.proj = MultiKrzProjection(cov.matrices, num.cores = 12, full.results = F)
-qplot(value, data = comparisons.proj, geom = "histogram")
