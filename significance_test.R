d83607dd97c835cf7bbe60d9cd89a60b2015b90b
diogro <diogro@gmail.com>
2014-06-11 12:11:45 -0300

    significance generic

--- significance_test.R
+++ significance_test.R
@@ -12,78 +12,65 @@ cov.matrices = lapply(nwm.matrices, function(x) x[[1]])
 # Significance Testing
 ###########################
 
+KrzProjSignificance <- function(x, y, randomCompFunc = c("RandomMat", "RandomEV", "ShuffleEV", "ShufflePop"), ..., iterations = 100){
+    randomCompFunc = match.arg(randomCompFunc)
+    if(randomCompFunc == "ShuffleEV"){
+        eigen_x <- eigen(x)
+        eigen_y <- eigen(y)
+    }
+    if(randomCompFunc == "RandomEV"){
+        evals <- eigen(x)$values
+    }
+    obs_sim <- KrzProjection(x, y)[[1]]
+    randomCompFunc = match.fun(randomCompFunc)
+    random_comps = aaply(1:iterations, 1, randomCompFunc, x, y, ..., .parallel = TRUE)
+    significance <- sum(random_comps > obs_sim)/iterations
+    return(c(SharedVariance = obs_sim, Prob = significance))
+}
+
 ## Null hipotesis is dissimilarity
 
 ## 1) Random Matrices
 
-KPS_RandomMatrix <- function(x, y, iterations = 100){
-    obs_sim <- KrzProjection(x, y)[[1]]
-    randomCompFunc = function(index){
-        rand_x <- RandomMatrix(dim(x)[[1]], variance = diag(x))
-        rand_y <- RandomMatrix(dim(y)[[1]], variance = diag(y))
-        KrzProjection(rand_x, rand_y)[[1]]
-    }
-    random_comps = aaply(1:iterations, 1, randomCompFunc, .parallel = TRUE)
-    significance <- sum(random_comps > obs_sim)/iterations
-    return(c(SharedVariance = obs_sim, Prob = significance))
+RandomMat <- function(index, x, y){
+    n = dim(x)[1]
+    rand_x <- RandomMatrix(n, variance = diag(x))
+    rand_y <- RandomMatrix(n, variance = diag(y))
+    KrzProjection(rand_x, rand_y)[[1]]
 }
 
 ## 2) Random EigenVectors
 
-KPS_RandomEV <- function(x, y, iterations = 100, size = TRUE){
-    obs_sim <- KrzProjection(x, y)[[1]]
-    n = dim(x)[1]
-    evals <- diag(eigen(x)$values)
-    randomCompFunc = function(index){
-        if(size) random_eVecs = qr.Q(qr(matrix(c(rep(1, n), rnorm(n*(n-1))), n, n)))
-        else     random_eVecs = qr.Q(qr(matrix(rnorm(n*n), n, n)))
-        rand_x =  t(random_eVecs) %*% evals %*% random_eVecs
-        KrzProjection(rand_x, y)[[1]]
-    }
-    random_comps = aaply(1:iterations, 1, randomCompFunc, .parallel = TRUE)
-    significance <- sum(random_comps > obs_sim)/iterations
-    return(c(SharedVariance = obs_sim, Prob = significance))
+RandomEV <- function(index, x, y, size = TRUE){
+    if(size) random_eVecs = qr.Q(qr(matrix(c(rep(1, n), rnorm(n*(n-1))), n, n)))
+    else     random_eVecs = qr.Q(qr(matrix(rnorm(n*n), n, n)))
+    rand_x =  t(random_eVecs) %*% evals %*% random_eVecs
+    KrzProjection(rand_x, y)[[1]]
 }
 
 ## 3) Shuffled eigenvectors
 
-KPS_ShuffleEigenVector <- function(x, y, iterations = 100){
+ShuffleEV <- function(index, x, y){
     n = dim(x)[1]
-    obs_sim <- KrzProjection(x, y)[[1]]
-    eigen_x <- eigen(x)
-    eigen_y <- eigen(y)
-    randomCompFunc = function(index){
-        shuffle_x = sample(1:n, n)
-        shuffle_y = sample(1:n, n)
-        rand_x = t(eigen_x$vectors[,shuffle_x]) %*% diag(eigen_x$values) %*% eigen_x$vectors[,shuffle_x]
-        rand_y = t(eigen_y$vectors[,shuffle_y]) %*% diag(eigen_y$values) %*% eigen_y$vectors[,shuffle_y]
-        KrzProjection(rand_x, rand_y)[[1]]
-    }
-    random_comps = aaply(1:iterations, 1, randomCompFunc, .parallel = TRUE)
-    significance <- sum(random_comps > obs_sim)/iterations
-    return(c(SharedVariance = obs_sim, Prob = significance))
+    shuffle_x = sample(1:n, n)
+    shuffle_y = sample(1:n, n)
+    rand_x = t(eigen_x$vectors[,shuffle_x]) %*% diag(eigen_x$values) %*% eigen_x$vectors[,shuffle_x]
+    rand_y = t(eigen_y$vectors[,shuffle_y]) %*% diag(eigen_y$values) %*% eigen_y$vectors[,shuffle_y]
+    KrzProjection(rand_x, rand_y)[[1]]
 }
-
 
 ## Null hipotesis is similarity
 
 ## 1) Random populations are shuffled
 
-KPS_ShufflePop = function(x, y, sample.x = 100, sample.y = 100, iterations = 100){
-    n = dim(x)[1]
-    obs_sim <- KrzProjection(x, y)[[1]]
-    randomCompFunc = function(index){
-        pop_x = data.frame(mvtnorm::rmvnorm(n = sample.x, sigma = x))
-        pop_x$'pop' = 'x'
-        pop_y = data.frame(mvtnorm::rmvnorm(n = sample.y, sigma = y))
-        pop_y$'pop' = 'y'
-        pop = rbind(pop_x, pop_y)
-        shuffle = sample(dim(pop)[1])
-        pop$'pop' = pop$'pop'[shuffle]
-        rand.P = dlply(pop, .(pop), function(x) cov(x[-dim(x)[2]]))
-        KrzProjection(rand.P)[1,2]
-    }
-    random_comps = aaply(1:iterations, 1, randomCompFunc, .parallel = TRUE)
-    significance <- sum(random_comps < obs_sim)/iterations
-    return(c(SharedVariance = obs_sim, Prob = significance))
+ShufflePop <- function(index, x, y, sample.x = 100, sample.y = 100){
+    pop_x = data.frame(mvtnorm::rmvnorm(n = sample.x, sigma = x))
+    pop_x$'pop' = 'x'
+    pop_y = data.frame(mvtnorm::rmvnorm(n = sample.y, sigma = y))
+    pop_y$'pop' = 'y'
+    pop = rbind(pop_x, pop_y)
+    shuffle = sample(dim(pop)[1])
+    pop$'pop' = pop$'pop'[shuffle]
+    rand.P = dlply(pop, .(pop), function(x) cov(x[-dim(x)[2]]))
+    KrzProjection(rand.P)[1,2]
 }
