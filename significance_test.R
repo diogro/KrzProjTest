f508059eb0680019f006a50ec3f810965be5662f
Edgar Z. Alvarenga <edgar@ymonad.com>
2014-06-27 18:00:23 -0300

    Using compFunc in randomCompFunc and pass argument if any to it

--- significance_test.R
+++ significance_test.R
@@ -14,7 +14,7 @@ cov.matrices = lapply(nwm.matrices, function(x) x[[1]])
 
 funcSignificance <- function(x, y, randomCompFunc, ..., compFunc=KrzProjection, iterations = 100) {
     obs_sim <- compFunc(x, y)[[1]]
-    random_comps = aaply(1:iterations, 1, function (i) randomCompFunc(x, y, ...), .parallel = TRUE)
+    random_comps = aaply(1:iterations, 1, function (i) randomCompFunc(x, y, compFunc, ...), .parallel = TRUE)
     significance <- sum(random_comps > obs_sim)/iterations
     return (c(SharedVariance = obs_sim, Prob = significance))
 }
@@ -23,11 +23,11 @@ funcSignificance <- function(x, y, randomCompFunc, ..., compFunc=KrzProjection, 
 
 ## 1) Random Matrices
 
-RandomMat <- function(x, y, compFunc=KrzProjection){
+RandomMat <- function(x, y, compFunc=KrzProjection, ...){
     n = dim(x)[1]
     rand_x <- RandomMatrix(n, variance = diag(x))
     rand_y <- RandomMatrix(n, variance = diag(y))
-    return (compFunc(rand_x, rand_y)[[1]])
+    return (compFunc(rand_x, rand_y, ...)[[1]])
 }
 
 ## 2) Random EigenVectors
@@ -81,10 +81,11 @@ ShufflePop <- function(x, y, compFunc=KrzProjection, sample.x = 100, sample.y = 
     return (compFunc(rand.P)[1,2])
 }
 
-rand_x = RandomMatrix(39, 2, 1, 10)
-rand_y = RandomMatrix(39, 2, 1, 10)
+rand_x = RandomMatrix(15, 2, 1, 10)
+rand_y = RandomMatrix(15, 2, 1, 10)
 
-sig_dist_RandMat = Map(function(x, y) funcSignificance(x, y, RandomMat, compFunc=PCAsimilarity), rand_x, rand_y)
+sig_dist_RandMat = Map(function(x, y) funcSignificance(x, y, RandomMat, ret.dim=15, compFunc=PCAsimilarity), rand_x, rand_y)
+kr_sig_dist_RandMat = Map(function(x, y) funcSignificance(x, y, RandomMat, compFunc=KrzProjection), rand_x, rand_y)
 #sig_dist_RandEV = Map(function(x, y) funcSignificance(x, y, RandomEV, diag(eigen(x)$values)), rand_x, rand_y)
 #sig_dist_ShuffleEV = Map(function(x, y) funcSignificance(x, y, ShuffleEV, eigen(x), eigen(y)), rand_x, rand_y)
 #sig_dist_ShufflePop = Map(function(x, y) funcSignificance(x, y, ShufflePop), rand_x, rand_x)
